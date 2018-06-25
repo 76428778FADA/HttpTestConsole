95593284e1b2451dc97efc9c9ce4f284142f9afe
Dan Patching <danpatching@protonmail.com>
2018-06-25 11:02:39 +0100

    console app for testing http connections

---
+++ HttpTestConsole/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Text;
+
+namespace HttpTestConsole
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (var http = new WebClient())
+			{
+				http.Headers.Add("Content-Type", "application/json; charset=utf-8");
+				http.DownloadDataCompleted += Http_DownloadDataCompleted;
+
+				var uri = new Uri("http://itashelp.hivedome.net/umbraco/api/helpapi/GetFieldHelpWithCulture?id=01024&c=en");
+				http.DownloadDataAsync(uri);
+			}
+
+			Console.WriteLine("-----------------------------");
+			Console.WriteLine("Press ENTER to EXIT");
+			Console.WriteLine("-----------------------------");
+			Console.ReadLine();
+		}
+
+		private static void Http_DownloadDataCompleted(object sender, DownloadDataCompletedEventArgs e)
+		{
+			if (e.Error != null)
+			{
+				Console.WriteLine("WEB REQUEST ERROR!");
+				Console.WriteLine(e.Error.ToString());
+			}
+			else
+			{
+				Console.WriteLine("WEB REQUEST SUCCESS!");
+				Console.WriteLine(Encoding.UTF8.GetString(e.Result));
+			}
+		}
+	}
+}
